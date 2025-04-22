07f899cf6c1fbddc31608e2dba702461daa053b1
mav01-code <marreddyakshayavarshini@gmail.com>
2025-04-22 17:20:01 +0530

    AAT Project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Scroll Reveal Animation
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.hero', { duration: 2000, origin: 'bottom', distance: '100px' });
+sr.reveal('.gallery', { duration: 2000, origin: 'left', distance: '100px' });
+sr.reveal('.about', { duration: 2000, origin: 'right', distance: '100px' });
+sr.reveal('.contact', { duration: 2000, origin: 'bottom', distance: '100px' });
